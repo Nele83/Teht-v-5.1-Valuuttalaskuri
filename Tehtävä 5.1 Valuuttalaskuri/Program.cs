144b23de2ab9d3e25e24c9f44743b1edba57127d
Nele83 <98733060+Nele83@users.noreply.github.com>
2022-02-21 13:14:08 +0200

    Tehtävä 5.1

--- Tehtävä 5.1 Valuuttalaskuri/Program.cs
+++ Tehtävä 5.1 Valuuttalaskuri/Program.cs
@@ -21,11 +21,14 @@ namespace Tehtävä_5._1_Valuuttalaskuri
             luku = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
 
-            //perustettu oma alueen {} luku 1 on dollari. muutetaan luku -> String.Format käskyllä stringiksi ja laskukaava sekä + $
+            //if:llä aletaan tarkistelemaan lukua. int muuttjuhalla on määritelty arvo joka on luku ja tässä ifi:llä
+            //todetaan, että luku on yhtäkun  numero 1:i.perustettu oma alueen {} luku 1 on dollari. 
+            //muutetaan luku -> String.Format käskyllä stringiksi ja laskukaava sekä + $. 
             if (luku == 1)
             {
                 Console.WriteLine(String.Format("{0:0.00}",valuutta * 1.21) + " $ ");
             }
+            //
             else if (luku == 2)
             {
                 Console.WriteLine(String.Format("{0:0.00}",valuutta * 0.87) + " £ ");
