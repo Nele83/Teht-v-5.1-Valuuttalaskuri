08d03d9a595d999ecb4b366d5bddb58498143d6a
Nele83 <98733060+Nele83@users.noreply.github.com>
2022-02-21 13:12:57 +0200

    Tehtävä 5.1

---
+++ Tehtävä 5.1 Valuuttalaskuri/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tehtävä_5._1_Valuuttalaskuri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esitellään muuttujat, henkilö valitsee
+            int luku;
+            double valuutta;
+            Console.WriteLine("VALUUTTALASKURI");
+            Console.WriteLine("---------------");
+            Console.WriteLine();
+            Console.Write("Anna summa euroina? ");
+            // Kysytty henkilöltä, muunettu ja tallennettu muuttujan arvoksi
+            valuutta = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Vastaa 1 jos haluat muuntaa dollareiksi ja 2 jos haluat muuntaa punniksi? ");
+            
+            //Kysytty henkilöltä 1 tai 2 ja merkkijono muutetaan kokonaisluvuksi ja tallenetaan lukuun
+            luku = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            //perustettu oma alueen {} luku 1 on dollari. muutetaan luku -> String.Format käskyllä stringiksi ja laskukaava sekä + $
+            if (luku == 1)
+            {
+                Console.WriteLine(String.Format("{0:0.00}",valuutta * 1.21) + " $ ");
+            }
+            else if (luku == 2)
+            {
+                Console.WriteLine(String.Format("{0:0.00}",valuutta * 0.87) + " £ ");
+            }
+            
+
+
+        }
+    }
+}
